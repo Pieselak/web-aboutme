2811a220f4ffe8760035dcdab0c4f1dab46ba81d
Pieselak <112515031+Pieselak@users.noreply.github.com>
2024-07-14 19:33:05 +0200

    Add files via upload

---
+++ pieselakme/php/database.php
@@ -0,0 +1,11 @@
+<?php
+$host = '';
+$user = '';
+$password = '';
+$database = '';
+
+$connection = mysqli_connect($host, $user, $password, $database);
+
+if (!$connection) {
+    die('Connection failed: ' . mysqli_connect_error());
+}
